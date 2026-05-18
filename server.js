3867247336d2d162e75eb94f0c28600c40a9e88a
BANERJEE NEIL SANDEEP <neilbanerjee2007@gmail.com>
2026-05-18 12:42:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json({ limit: '1mb' }));
 app.use(
   rateLimit({
     windowMs: 60 * 1000,
-    max: 100,
+    max: 1000,
     standardHeaders: true,
     legacyHeaders: false
   })
